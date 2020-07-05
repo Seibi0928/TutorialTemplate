8fefb40969bf8f39f331247b73a6953049207f92
e a <nanteneus@hotmail.co.jp>
2020-07-05 08:00:48 +0000

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+﻿import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+export class Hello extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World!!!</h1>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
